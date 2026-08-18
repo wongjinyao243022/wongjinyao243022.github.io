05cb53da8463d420b702cd348ad19c47d78eaf9c
Lim Zhi Ian <youremail@yourdomain.com>
2026-08-18 10:09:56 +0800

    feat: open a new js file (footer) and add animation into marketing strategy footer section

---
+++ web/js/footer.js
@@ -0,0 +1,12 @@
+gsap.to(".footer-subscribe", {
+    scrollTrigger: {
+        trigger: ".section",
+        start: "top 20%" /* target, screen*/,
+        scrub: true,
+        markers: 2
+    },
+    x: 500, // move 200px to the right
+    opacity: 0.5, // fade to 50% opacity
+    duration: 1.5, // 1.5 seconds
+    ease: "power2.out"
+});
