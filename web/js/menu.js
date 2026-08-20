452e5e79551c40f967b1aac8326359e71a473091
Koo May Quin <mayquin243020@dasein.edu.my>
2026-08-20 15:28:58 +0800

    fix ： adjust header padding

--- web/js/menu.js
+++ web/js/menu.js
@@ -57,3 +57,71 @@ overlayItems.forEach((item) => {
     });
 
 });
+
+/* ================================
+   OPEN LAPTOP MENU
+================================ */
+
+const menuToggle =
+    document.getElementById("menu-toggle-burger");
+
+const laptopMenu =
+    document.getElementById("laptop-menu");
+
+const closeMenu =
+    document.getElementById("close-menu");
+
+
+menuToggle.addEventListener("click", function () {
+
+    laptopMenu.classList.add("active");
+
+});
+
+
+/* ================================
+   CLOSE LAPTOP MENU
+================================ */
+
+closeMenu.addEventListener("click", function () {
+
+    laptopMenu.classList.remove("active");
+
+});
+
+
+/* ================================
+   EXPERTISE SUBMENU
+================================ */
+
+const mobileMenuLinks =
+    document.querySelectorAll(".mobile-menu-link");
+
+
+mobileMenuLinks.forEach(function (button) {
+
+    button.addEventListener("click", function () {
+
+        const submenu =
+            button.nextElementSibling;
+
+        const arrow =
+            button.querySelector(".mobile-arrow p");
+
+
+        submenu.classList.toggle("active");
+
+
+        if (submenu.classList.contains("active")) {
+
+            arrow.textContent = "⌃";
+
+        } else {
+
+            arrow.textContent = "⌄";
+
+        }
+
+    });
+
+});
